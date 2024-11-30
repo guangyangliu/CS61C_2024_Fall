bbf87e3aded32966decca237ec048660d465f653
RisingUppercut <2858283548@qq.com>
2024-11-30 15:28:18 +0800

    done with lab04

--- lab04/ex3.s
+++ lab04/ex3.s
@@ -32,19 +32,28 @@ main:
 #     where ^ is the exponent operator, not XOR
 ex3:
     # Note: Add code BELOW without altering existing lines.
-
+    
+    
     # return 1 if a1 == 0
     beq a1 x0 ex3_zero_case
+    
+    addi sp, sp -8
+    sw t0, 0(sp)
+    sw ra, 4(sp)
+    
 
     # otherwise, return ex3(a0, a1-1) * a0
     mv t0 a0      # save a0 in t0
     addi a1 a1 -1 # decrement a1
-
+    
     jal ra ex3    # call ex3(a0, a1-1)
 
     mul a0 a0 t0  # multiply ex3(a0, a1-1) by t0
                   # (which contains the value of a0)
-
+    
+    lw t0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
     j ex3_end
 
     # Note: Add code ABOVE without altering existing lines.
